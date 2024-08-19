4387b2df66207994351024b2b6917044c13fd2f9
imosta <imostainn@gmail.com>
2024-08-19 08:24:22 +0300

    version 0.1.0

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { AppContext } from "@/context";
+import { FiLogOut } from "react-icons/fi";
 
 const Header = ({ title, subtitle }) => {
   const { setToBeSigned, setCurrentCycle } = AppContext();
@@ -35,7 +36,7 @@ const Header = ({ title, subtitle }) => {
     <div className="cover w-full">
       <div className="bg-[#B18F13] bg-opacity-50 h-full">
         <div className="container max-w-[90%] h-full mx-auto">
-          <div className="flex flex-row justify-between pt-10">
+          <div className="flex flex-col items-center lg:flex-row justify-center lg:justify-between pt-10">
             <Link href="https://rocaaccountants.co.uk/" className="w-[100px]">
               <Image
                 src="/logo.png"
@@ -48,15 +49,16 @@ const Header = ({ title, subtitle }) => {
             {userData && (
               <div>
                 <div className="items-center">
-                  <p className="text-white border-2 border-white inline-block p-5 rounded-lg text-[10px] lg:text-[20px] mr-10 bg">
+                  <p className="text-white border-2 border-white inline-block p-[15px] rounded-lg text-[10px] lg:text-[20px] mr-[20px]">
                     {userData.user?.companyName}
                   </p>
 
                   <button
-                    className="bg-[#B18F13] py-[15px] px-[30px] rounded-full text-white"
+                    className="bg-[#B18F13] p-[15px] lg:p-[20px] rounded-lg text-white text-[10px] lg:text-[18px]"
                     onClick={onSignOut}
                   >
-                    Sign Out
+                    <span className="">Sign out</span>
+                    <FiLogOut className="inline-block ml-[10px]"></FiLogOut>
                   </button>
                 </div>
               </div>
@@ -64,7 +66,7 @@ const Header = ({ title, subtitle }) => {
           </div>
 
           <div className="py-[60px] text-center">
-            <h1 className="text-[24px] lg:text-[48px]  font-semibold text-white">
+            <h1 className="text-[24px] lg:text-[48px] font-semibold text-white">
               {title ? title : "Customer Portal"}
             </h1>
             <p className="font-medium text-[24px] text-white">{subtitle}</p>
